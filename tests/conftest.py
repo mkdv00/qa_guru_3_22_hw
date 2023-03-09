00e8a01958249054e9b1e3fd2b487f797df1b758
Maksim Kudaev <kudaev.m@nakukop.com>
2023-03-09 11:54:41 +0300

    add: last api homework

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import os
+
+import allure
+import pytest
+from dotenv import load_dotenv
+from framework.demo_qa import DemoQaWithEnv
+from selene.support.shared import browser
+
+browser.config.base_url = "https://demowebshop.tricentis.com"
+
+load_dotenv()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--env", action='store', default="prod")
+
+
+@pytest.fixture(scope='session')
+def demoshop(request):
+    env = request.config.getoption("--env")
+    return DemoQaWithEnv(env)
+
+
+@pytest.fixture(scope='session')
+def reqres(request):
+    env = request.config.getoption("--env")
+    return DemoQaWithEnv(env).reqres
+
+
+@pytest.fixture(scope='function')
+def auth_browser(demoshop):
+    response = demoshop.login(os.getenv("LOGIN"), os.getenv("PASSWORD"))
+    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+
+    with allure.step("Check code"):
+        response.status_code = 302
+
+    browser.open("/Themes/DefaultClean/Content/images/logo.png")
+
+    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
+    return browser
---
+++ tests/ui_demoshop/test_for_demoshop.py
@@ -0,0 +1,52 @@
+import os
+
+import allure
+from dotenv import load_dotenv
+from selene import have
+from selene.support.shared import browser
+
+load_dotenv()
+
+
+def test_login_with_api(auth_browser):
+    auth_browser.open("")
+
+    with allure.step("Verify successful authorization"):
+        auth_browser.element(".account").should(have.text(os.getenv("LOGIN")))
+
+
+def test_search_negative_result(auth_browser):
+    auth_browser.open("")
+    with allure.step("Negitive search"):
+        browser.element('.search-box [value="Search store"]').click()
+        browser.element('.search-box [value="Search store"]').type('negative test').press_enter()
+        browser.element('.result').should(have.text('No products were found that matched your criteria.'))
+
+
+def test_logout(auth_browser):
+    auth_browser.open("")
+    with allure.step("Check logout"):
+        browser.element('.ico-logout').click()
+        browser.element('.ico-login').should(have.text('Log in'))
+
+
+def test_watch_profile(auth_browser):
+    auth_browser.open("")
+
+    with allure.step("Check info in profile"):
+        browser.element(".account").should(have.text(os.getenv('LOGIN'))).click()
+        browser.element('#FirstName').should(have.value('mkdv'))
+        browser.element('#LastName').should(have.value('00'))
+        browser.element('#Email').should(have.value(os.getenv('LOGIN')))
+        browser.element('[checked="checked"]#gender-male')
+
+
+def test_watch_page_change_password(auth_browser):
+    auth_browser.open("")
+
+    with allure.step("Check text buttion in change password"):
+        browser.element(".account").should(have.text(os.getenv('LOGIN'))).click()
+        browser.element('[href="/customer/changepassword"]').should(have.text('Change password')).click()
+        browser.element('[for="OldPassword"]').should(have.text('Old password:'))
+        browser.element('[for="NewPassword"]').should(have.text('New password:'))
+        browser.element('[for="ConfirmNewPassword"]').should(have.text('Confirm password:'))
